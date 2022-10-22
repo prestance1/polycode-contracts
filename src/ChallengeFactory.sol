088653b7ee2e8b0a8877af482ac59c8c8d6a804e
prestance1 <oudrhiri.amin@gmail.com>
2022-10-22 17:19:24 +0100

    Add State variables for challenge factory

--- src/ChallengeFactory.sol
+++ src/ChallengeFactory.sol
@@ -1,3 +1,18 @@
 pragma solidity ^0.8.13;
 
-contract ChallengeFactory {}
+contract ChallengeFactory {
+    address immutable rewardToken;
+    mapping(address => uint256) public getId;
+    mapping(uint256 => address) public getChallenge;
+    address[] public allChallenges;
+
+    event ChallengeCreated(uint256 indexed challengeId, address indexed challenge);
+
+    constructor(address _rewardToken) {
+        rewardToken = _rewardToken;
+    }
+
+    function allChallengesLength() external view returns (uint256) {
+        return allChallenges.length;
+    }
+}
