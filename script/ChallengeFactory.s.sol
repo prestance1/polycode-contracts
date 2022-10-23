041f5962a0c7c53a1cc2c28315da3cd35e57bb97
prestance1 <oudrhiri.amin@gmail.com>
2022-10-23 03:02:57 +0100

    :sparkles: Finishing Touches

--- script/ChallengeFactory.s.sol
+++ script/ChallengeFactory.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {ChallengeFactory} from "../src/ChallengeFactory.sol";
 
-contract TokenScript is Script {
+contract FactoryScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
